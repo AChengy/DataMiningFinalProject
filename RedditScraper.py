c0ebe241bc6cdfeb59a6407dfa3c9395fac82129
Alex Chengelis <alex.chengelis@gmail.com>
2016-08-02 01:08:45 -0400

    initial commit

---
+++ RedditScraper.py
@@ -0,0 +1,27 @@
+
+import praw
+f = open("top25submissioncomments.txt", 'w')
+r = praw.Reddit(user_agent='my_cool_application')
+submissions = r.get_subreddit('nba').get_top_from_year(limit = 25)
+
+for submission in submissions:
+    print (submission.title)
+    f.write(submission.title)
+    #print (submission.url)
+    print ("=" * 30)
+    f.write("=" * 30)
+    #for sorting all the comments by score
+    #submission.comments = sorted(submission.comments, key=lambda x: x.score)
+    submission.replace_more_comments(limit=None, threshold=0)
+    all_comments = submission.comments
+    for comment in all_comments:
+        print (comment.body)
+        f.write(comment.body)
+        print ("score: " + str(comment.score))
+        f.write("score: " + str(comment.score))
+        print ("#" * 10)
+        f.write("#" * 10)
+    print ("\n" * 3)
+    f.write("\n" * 3)
+
+f.close()
