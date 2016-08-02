23840f8f1dcdecd0f621a3bdea7c2f9385440ed6
Alex Chengelis <alex.chengelis@gmail.com>
2016-08-02 01:47:10 -0400

    Figured out flairs and Urls.

--- RedditScraper.py
+++ RedditScraper.py
@@ -1,28 +1,53 @@
-
 import praw
-f = open("top25submissioncomments.txt", 'w')
+
+urls = ['https://www.reddit.com/r/nba/comments/4owimb/post_game_thread_the_cleveland_cavaliers_defeat/']
+
+#f = open("top25submissioncomments.txt", 'w')
 r = praw.Reddit(user_agent='my_cool_application')
-submissions = r.get_subreddit('nba').get_top_from_year(limit = 25)
 
-for submission in submissions:
-    print (submission.title)
-    f.write(submission.title,"\n")
-    #print (submission.url)
-    print ("=" * 30)
-    f.write("=" * 30)
-    #for sorting all the comments by score
-    #submission.comments = sorted(submission.comments, key=lambda x: x.score)
+for url in urls:
+    submission = r.get_submission(url)
+    print(submission.title)
+    print("=" * 30)
+
     submission.replace_more_comments(limit=None, threshold=0)
+    # for sorting all the comments by score
     all_comments = submission.comments
-    for comment in all_comments:
-        print (comment.body)
-        #currently runs into an encoding problem on some comments.
-        #f.write("\n",comment.body)
-        print ("score: " + str(comment.score))
-        f.write("\nscore: " + str(comment.score),"\n")
-        print ("#" * 10)
-        f.write("#" * 10)
-    print ("\n" * 3)
-    f.write("\n" * 3)
+    all_comments = sorted(all_comments, key=lambda x: x.score)
 
-f.close()
+    for comment in all_comments:
+        print(comment.body)
+        print("score: " + str(comment.score))
+        print("flair: ", comment.author_flair_text)
+        print("#" * 10)
+    print("\n"*3)
+
+
+
+
+# submissions = r.get_subreddit('nba').get_top_from_year(limit = 25)
+#
+# for submission in submissions:
+#     print (submission.title)
+#     f.write(submission.title+"\n")
+#     #print (submission.url)
+#     print ("=" * 30)
+#     f.write("=" * 30)
+#     #for sorting all the comments by score
+#     #submission.comments = sorted(submission.comments, key=lambda x: x.score)
+#     #submission.replace_more_comments(limit=None, threshold=0)
+#     all_comments = submission.comments
+#     for comment in all_comments:
+#         print (comment.body)
+#         #currently runs into an encoding problem on some comments.
+#         #f.write("\n",comment.body)
+#         print ("score: " + str(comment.score))
+#         f.write("\nscore: " + str(comment.score) + "\n")
+#
+#         print("flair: ", comment.author_flair_text)
+#         print ("#" * 10)
+#         f.write("#" * 10)
+#     print ("\n" * 3)
+#     f.write("\n" * 3)
+#
+# f.close()
